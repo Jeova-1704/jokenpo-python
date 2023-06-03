24caf6c3acbeb90f7912e7db362e7f08a130d213
Jeova BL <jeovab115@gmail.com>
2023-06-03 17:20:44 -0300

    Refatoração e aprimoramento do codigo

--- funcionalidade.py
+++ funcionalidade.py
@@ -9,22 +9,29 @@ class Forca:
         self.pontos_jogador = 0
         self.quantidade_de_jogadas = 0
         #escolha da jogada 
-        self.escolha = 0
+        self.escolha_j = 0
         self.escolha_pc = 0
     
-    def cabecalho(self):
-        titulo = 'JOKEMPÔ'
+    def cabecalho(self, titulo):
+        if titulo == 'titulo':
+            titulo = 'JOKEMPÔ'
+        else:
+            titulo = titulo
+        vitorias = f"Vitorias: {self.pontos_jogador}"
+        derrotas = f"Derrotas: {self.pontos_pc}"
+        empates = f"Empates: {self.empate}"
+        encerramento = f"para encerrar digite: 999"
         print("╔═════════════════════════════════════════════════════╗")
         print(f"║{titulo.center(53)}║")
         print("╠═════════════════════════════════════════════════════╣")
         print("╠═════════════════════════════════════════════════════╣")
-        print(f"║       Vitorias: {self.pontos_jogador}                Derrotas: {self.pontos_pc}        ║")
+        print(f"║{vitorias.center(26)} {derrotas.center(26)}║")
         print("║                                                     ║")
-        print(f"║       Empates: {self.empate}       Para sair digite: 999        ║")
+        print(f"║{empates.center(53)}║")
         print("║                                                     ║")
         print("╚═════════════════════════════════════════════════════╝")
 
-    def pedra(self):
+    def __pedra(self):
         print("    _______")
         print("---'   ____)")
         print("      (_____)")
@@ -32,7 +39,7 @@ class Forca:
         print("      (____)")
         print("---.__(___)")
 
-    def papel(self):
+    def __papel(self):
         print("    _______")
         print("---'   ____)____")
         print("          ______)")
@@ -40,62 +47,175 @@ class Forca:
         print("         _______)")
         print("---.__________)")
 
-    def tesoura(self):
+    def __tesoura(self):
         print("    _______")
         print("---'   ____)____")
         print("          ______)")
         print("       __________)")
         print("      (____)")
         print("---.__(___)")
-    
-    def versus(self):
-        print("V    V  SSSS")
-        print("V    V S")
-        print("V    V  SSS")
-        print(" V  V      S")
-        print("  V    SSSS")
 
     def escolha_Do_Jogador(self):
 
         while True:
             print('Escolha a sua jogada:\n [1] PEDRA\n [2] PAPEL\n [3]  TESOURA')
-            valor = input()
+            valor = input('→')
             if valor in '123':
                 try:
-                    self.escolha = int(valor)
+                    self.escolha_j = int(valor)
                     break
                 except ValueError:
+                    self.cleamTime()
+                    self.cabecalho('titulo')
                     print("Digite um valor válido e inteiro.")
             else:
-                print("Por favor, escolha um valor válido acima.")
+                self.cleamTime()
+                self.cabecalho('titulo')
+                print("Por favor, escolha um valor abaixo.")
 
     def escolha_Do_pc(self):
         pc = [1, 2, 3]
         self.escolha_pc = choice(pc)
 
-    def cleamTime(self, time=0, clear=False):
-        time.sleep(time)
-        if clear:
-            os.system("cls")
+    def cleamTime(self, segundos=0):
+        time.sleep(segundos)
+        os.system("cls")
     
     def vencedor(self):
-        
+        self.cleamTime()
+        if self.escolha_j == 1 and self.escolha_pc == 1:
+            #se player escolher PEDRA e pc escolher pedra
+            print('-=-'*10)
+            print('JOGADOR:')
+            self.__pedra()
+            print('PC:')
+            self.__pedra()
+            print('-=-'*10)
+            self.empate += 1
+            self.cleamTime(3)
+            self.cabecalho("O jogo deu empate")
 
+        elif self.escolha_j == 1 and self.escolha_pc == 2:  
+            # se player escolher PEDRA e pc escolher papel
+            print('-=-'*10)
+            print('JOGADOR:')
+            self.__pedra()
+            print('PC:')
+            self.__papel()
+            print('-=-'*10)
+            self.pontos_pc += 1
+            self.cleamTime(3)
+            self.cabecalho("O pc ganhou")
 
+        elif self.escolha_j == 1 and self.escolha_pc == 3:
+            # se player escolher PEDRA e pc escolher tesoura
+            print('-=-'*10)
+            print('JOGADOR:')
+            self.__pedra()
+            print('PC:')
+            self.__tesoura()
+            print('-=-'*10)
+            self.pontos_jogador += 1
+            self.cleamTime(3)
+            self.cabecalho("O jogador ganhou")
 
+        elif self.escolha_j == 2 and self.escolha_pc == 1:
+            #se player escolher PAPEL e pc escolher PEDRA
+            print('-=-'*10)
+            print('JOGADOR:')
+            self.__papel()
+            print('PC:')
+            self.__pedra()
+            print('-=-'*10)
+            self.pontos_jogador += 1
+            self.cleamTime(3)
+            self.cabecalho("O jogador ganhou")
 
+        elif self.escolha_j == 2 and self.escolha_pc == 2:#se player escolher PAPEL e pc escolher PAPEL
+            print('-=-'*10)
+            print('JOGADOR:')
+            self.__papel()
+            print('PC:')
+            self.__papel()
+            print('-=-'*10)
+            self.empate += 1
+            self.cleamTime(3)
+            self.cabecalho("O jogo deu empate")
 
+        elif self.escolha_j == 2 and self.escolha_pc == 3:
+            # se player escolher PAPEL e pc escolhe tesoura
+            print('-=-'*10)
+            print('JOGADOR:')
+            self.__papel()
+            print('PC:')
+            self.__tesoura()
+            print('-=-'*10)
+            self.pontos_pc += 1
+            self.cleamTime(3)
+            self.cabecalho("O pc ganhou")
+
+        elif self.escolha_j == 3 and self.escolha_pc == 1:
+            #se player escolher TESOURA e pc escolhe PEDRA
+            print('-=-'*10)
+            print('JOGADOR:')
+            self.__tesoura()
+            print('PC:')
+            self.__pedra()
+            print('-=-'*10)
+            self.pontos_pc += 1
+            self.cleamTime(3)
+            self.cabecalho("O pc ganhou")
+
+        elif self.escolha_j == 3 and self.escolha_pc == 2:  # se player escolher TESOURA e pc escolhe PAPEL
+            print('-=-'*10)
+            print('JOGADOR:')
+            self.__tesoura()
+            print('PC:')
+            self.__papel()
+            print('-=-'*10)
+            self.pontos_jogador += 1
+            self.cleamTime(3)
+            self.cabecalho("O jogador ganhou")
+
+        elif self.escolha_j == 3 and self.escolha_pc == 3:  # se player escolher TESOURA e pc escolhe TESOURA
+            print('-=-'*10)
+            print('JOGADOR:')
+            self.__tesoura()
+            print('PC:')
+            self.__tesoura()
+            print('-=-'*10)
+            self.empate += 1
+            self.cleamTime(3)
+            self.cabecalho("O jogo deu empate")
+    
+    def encerramento(self):
+        self.cleamTime()
+        self.cabecalho('END GAME! Resultado final:')
+
+    def jogarNovamente(self):
+        jogarDeNovo = input("Você deseja jogar novamente [S/N]").lower().strip()
+        while jogarDeNovo not in ['s', 'n', 'sim', 'não']:
+            jogarDeNovo = input("Resposta inválida. Por favor, responda novamente [S/N]").lower().strip()
+        if jogarDeNovo in ['s', 'sim']:
+            self.cleamTime()
+            return True
+        else:
+            self.cleamTime()
+            self.encerramento()
+            self.cleamTime(3)
+            return False
 
 
 
 if __name__ == '__main__':
     obj = Forca()
-    obj.cabecalho()
+    obj.cabecalho('titulo')
     # obj.pedra()
     # obj.tesoura()
     # obj.versus()
     # obj.papel()
-    obj.escolha_Do_Jogador()
-    print(obj.escolha)
+    # obj.escolha_Do_Jogador()
+    # print(obj.escolha_j)
+    obj.jogarNovamente()
 
     
--- main.py
+++ main.py
@@ -1,96 +1,11 @@
-#Crie um programa que faça o computador jogar Jokenpô com você.
-from random import choice
-from time import sleep
+from funcionalidade import *
 
+forca = Forca()
+jogarDeNovo = True
 
-linha = "\033[1;31m-=-\033[m"
-titulo = '\033[1;32mJOGO DE JOKENPÔ\033[m'
-pontos_pc = 0
-pontos_jogador = 0
-empate = 0
-
-print(linha * 20)
-print(f"{titulo:^60}")
-print(linha * 20)
-
-while True:
-
-      print('Escolha a sua jogada:\n [1] PEDRA\n [2] PAPEL\n [3] TESOURA')
-      player = int(input('Qual o número de sua jogada? '))
-
-      pc = [1, 2, 3]#1=pedra #2=papel #3=tesoura
-      escolha = choice(pc)
-      print('JO...'), sleep(2), print('KEN...'), sleep(2), print('Pô!!!')
-      if player == 1 and escolha == 1:#se player escolher PEDRA e pc escolher pedra
-            print('-=-'*10)
-            print('JOGADOR = PEDRA')
-            print('  PC    = PEDRA')
-            print('-=-' * 10)
-            print('O JOGO DEU EMPATE!')
-            empate += 1
-      elif player == 1 and escolha == 2:  # se player escolher PEDRA e pc escolher papel
-            print('-=-' * 10)
-            print('JOGADOR = PEDRA')
-            print('  PC    = PAPEL')
-            print('-=-' * 10)
-            print('O PC GANHOU!')
-            pontos_pc +=1
-      elif player == 1 and escolha == 3:  # se player escolher PEDRA e pc escolher tesoura
-            print('-=-' * 10)
-            print('JOGADOR =  PEDRA')
-            print('  PC    = TESOURA')
-            print('-=-' * 10)
-            print('O JOGADOR VENCEU!')
-            pontos_jogador += 1
-      elif player == 2 and escolha == 1:#se player escolher PAPEL e pc escolher PEDRA
-            print('-=-' * 10)
-            print('JOGADOR = PAPEL')
-            print('  PC    = PEDRA')
-            print('-=-' * 10)
-            print('O JOGADOR VENCEU!')
-            pontos_jogador += 1
-      elif player == 2 and escolha == 2:#se player escolher PAPEL e pc escolher PAPEL
-            print('-=-' * 10)
-            print('JOGADOR = PAPEL')
-            print('  PC    = PAPEL')
-            print('-=-' * 10)
-            print('O JOGO DE EMPATE!')
-            empate += 1
-      elif player == 2 and escolha == 3:  # se player escolher PAPEL e pc escolhe tesoura
-            print('-=-' * 10)
-            print('JOGADOR =  PAPEL')
-            print('  PC    = TESOURA')
-            print('-=-' * 10)
-            print('O PC GANHOU!')
-            pontos_pc += 1
-      elif player == 3 and escolha == 1:#se player escolher TESOURA e pc escolhe PEDRA
-            print('-=-' * 10)
-            print('JOGADOR = TESOURA')
-            print('  PC    = PEDRA')
-            print('-=-' * 10)
-            print('O PC GANHOU!')
-            pontos_pc += 1
-      elif player == 3 and escolha == 2:  # se player escolher TESOURA e pc escolhe PAPEL
-            print('-=-' * 10)
-            print('JOGADOR = TESOURA')
-            print('  PC    = PAPEL')
-            print('-=-' * 10)
-            print('O JOGADOR GANHOU!')
-            pontos_jogador += 1
-      elif player == 3 and escolha == 3:  # se player escolher TESOURA e pc escolhe TESOURA
-            print('-=-' * 10)
-            print('JOGADOR =  TESOURA')
-            print('  PC    = TESOURA')
-            print('-=-' * 10)
-            print('O JOGO DEU EMPATE!')
-            empate += 1
-      else:
-            print('Por favor insira uma opção válida!')
-      jogar_novamente = input("Deseja jogar novamente? (S/N) ")
-      if jogar_novamente.lower() == "n":
-        break
-print()
-print("PONTUAçÃO FINAL:")
-print(f'\033[1;32m{pontos_pc=}')
-print(f'{pontos_jogador=}')
-print(f'{empate=}\033[m')
+while jogarDeNovo:
+    forca.cabecalho('titulo')
+    forca.escolha_Do_Jogador()
+    forca.escolha_Do_pc()
+    forca.vencedor()
+    jogarDeNovo = forca.jogarNovamente()
